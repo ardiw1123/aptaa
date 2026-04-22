7736a94948dcad429c9bbb6e199da7a7019589ef
ardigunawanpratama@gmail.com <192996411+ardiw1123@users.noreply.github.com.>
2026-04-22 15:36:59 +0700

    fix postgresql boolean type error

--- app/Http/Controllers/AdminStokController.php
+++ app/Http/Controllers/AdminStokController.php
@@ -26,7 +26,7 @@ class AdminStokController extends Controller
                 DB::raw('COUNT(id) as total_macam_barang'),
                 DB::raw('SUM(jumlah_unit) as total_ekor'),
                 DB::raw('SUM(jumlah_berat) as total_berat'),
-                DB::raw('SUM(CASE WHEN is_verified = 0 THEN 1 ELSE 0 END) as jumlah_pending')
+                DB::raw('SUM(CASE WHEN is_verified = false THEN 1 ELSE 0 END) as jumlah_pending')
             )
             ->groupBy('tanggal_masuk')
             ->orderBy('tanggal_masuk', 'desc')
